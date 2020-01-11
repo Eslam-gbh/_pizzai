0aec3cd7bf36fbc70bf942f1c2ee418d64e6d9e6
eslam.gamal <slom72@gmail.com>
2020-01-11 13:44:12 +0200

    Add URLs

--- pizzai/urls.py
+++ pizzai/urls.py
@@ -6,16 +6,20 @@ from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 from .users.views import UserViewSet, UserCreateViewSet
+from .orders.views import OrderViewSet
+from rest_framework_swagger.views import get_swagger_view
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'users', UserCreateViewSet)
-
+router.register(r'orders', OrderViewSet)
+schema_view = get_swagger_view(title='Pizzai API')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
     path('api-token-auth/', views.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-docs/', schema_view),
 
     # the 'api-root' from django rest-frameworks default router
     # http://www.django-rest-framework.org/api-guide/routers/#defaultrouter
