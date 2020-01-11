ca6bc3fbd6a266825a843ceb59940662aa114d6e
eslam.gamal <slom72@gmail.com>
2020-01-11 13:39:12 +0200

    Modify Users Models

--- pizzai/users/models.py
+++ pizzai/users/models.py
@@ -11,6 +11,13 @@ from rest_framework.authtoken.models import Token
 @python_2_unicode_compatible
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    city_id = models.ForeignKey('City', on_delete=models.PROTECT, null=True)
+    address = models.TextField(null=True)
+    contact_phone = models.CharField(max_length=15, null=True)
+    email = models.EmailField(max_length=255, unique=True)
+    confirmation_code = models.CharField(max_length=255, null=True)
+    time_joined = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.username
@@ -20,3 +27,15 @@ class User(AbstractUser):
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+
+class City(models.Model):
+    class Meta:
+        verbose_name = "City"
+        verbose_name_plural = "Cities"
+
+    city_name = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.city_name
